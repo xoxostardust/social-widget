d6d69b128a8e213598edd1d6049afbf983d57874
gabs <risingmoons16@gmail.com>
2023-03-30 15:44:53 -0700

    trying to add menu and blend branches

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const icon = document.getElementById('icon');
+const music = document.getElementById('music');
+const musicProfile = document.getElementById('tracks');
+const homeProfile = document.getElementById('home');
 
-icon.addEventListener('mouseover', () => {
+music.addEventListener('click', () => {
     alert("i am crying in the club so very hard waa waaa uwu sad");
+    homeProfile.toggle('hide', true);
+    musicProfile.toggle('hide', false);
   }); 
+
+  // work in progress
+  // need to make it so that clicking will hide home and create music aaa
